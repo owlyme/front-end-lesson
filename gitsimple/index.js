277e683c4fd25ef40d54e09a924142a8b3a2a48c
owlyme <1148452777@qq.com>
2021-09-28 23:44:48 +0800

    test

--- gitsimple/index.js
+++ gitsimple/index.js
@@ -6,16 +6,16 @@ const git = simpleGit(gitPath, { binary: 'git' });
 
 async function run() {
     let res = await git.add(".")
-    console.log(res)
+    console.log(1, res)
 
     res = await git.commit("test")
-    console.log(res)
+    console.log(2, res)
 
     res = await git.status()
-    console.log(res)
+    console.log(3, res)
 
     res = await git.push("origin", "master")
-    console.log(res)
+    console.log(4, res)
 }
 
 run()
