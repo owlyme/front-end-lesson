baeda6469e28f28feba8c91d0031023d6c35b6f1
owlyme <1148452777@qq.com>
2021-09-29 00:02:27 +0800

    test

--- gitsimple/index.js
+++ gitsimple/index.js
@@ -5,21 +5,26 @@ const gitPath = path.join(__dirname, "../", )
 const git = simpleGit(gitPath, { binary: 'git' });
 
 async function run() {
-    let res = await git.branch()
-    console.log(0, res)
-    console.log(chalk.green(res.current))
+    try {
+        let res = await git.branch()
+        console.log(0, res)
+        console.log(chalk.green(res.current))
 
-    res = await git.add(".")
-    console.log(1, res)
+        res = await git.add(".")
+        console.log(1, res)
 
-    res = await git.commit("test")
-    console.log(2, res)
+        res = await git.commit("test")
+        console.log(2, res)
 
-    res = await git.status()
-    console.log(3, res)
+        res = await git.status()
+        console.log(3, res)
 
-    res = await git.push("origin", "master")
-    console.log(4, res)
+        res = await git.push("origin", "master")
+        console.log(4, res)
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 run()
