909e7f1e3706de57017a1f7a850f40498b98ec25
owlyme <1148452777@qq.com>
2021-09-28 23:56:48 +0800

    test

--- gitsimple/index.js
+++ gitsimple/index.js
@@ -8,7 +8,7 @@ async function run() {
 
     let res = await git.branch()
     console.log(0, res)
-    chalk.green(res.current)
+    console.log(res.current)
 
     res = await git.add(".")
     console.log(1, res)
